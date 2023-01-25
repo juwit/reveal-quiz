8e9c852fbd805f5e533d57305ba209553e0f0e9e
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-01-25 11:10:40 +0100

    ✨ : send last status when new client connects

--- server.mjs
+++ server.mjs
@@ -7,7 +7,15 @@ const io = new Server(3000, {
     },
 });
 
+let lastState = null;
+
 io.on( 'connection', socket => {
+
+    // also send to this new user the last status so it reconnects to the good slide
+    if(lastState){
+        console.log('New user, sending last status');
+        socket.emit(lastState.socketId, lastState);
+    }
 
     socket.on('user-connected', data => {
         console.log('Another user connected !');
@@ -15,9 +23,12 @@ io.on( 'connection', socket => {
         socket.broadcast.emit('user-connected', data);
     });
 
-
     socket.on('broadcast', data => {
         console.log(`received message ${JSON.stringify(data)}`);
+
+        if(data.state){
+            lastState = data;
+        }
 
         // checking that a secret is provided
         if (typeof data.secret == 'undefined' || data.secret == null || data.secret === '') return;
