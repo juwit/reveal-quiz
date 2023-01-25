c3d2e433cc3d9d43c0acdeba1870ed28faf71704
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-01-25 11:10:15 +0100

    🐛 : add missing prototypes to objects

--- src/service/quizService.ts
+++ src/service/quizService.ts
@@ -15,29 +15,31 @@ class QuizServiceImpl implements QuizService{
      */
     loadOrCreateQuiz(deck: Deck, role: Role): Quiz{
         // look in sessionStorage
-        const storage:Storage = window.sessionStorage;
+        const storage = window.sessionStorage;
         const savedQuiz = storage.getItem('quiz');
-        if(savedQuiz !== null){
+
+        let quiz;
+        if (savedQuiz !== null) {
             console.log('Loading the quiz from the session storage.');
-            const quiz = JSON.parse(savedQuiz);
+            const jsonQuiz = JSON.parse(savedQuiz);
             // add prototypes to objects (sad)
-            Object.setPrototypeOf(quiz, Quiz.prototype);
-            quiz.questions.forEach(question => {
+            Object.setPrototypeOf(jsonQuiz, Quiz.prototype);
+            jsonQuiz.questions.forEach(question => {
                 Object.setPrototypeOf(question, Question.prototype);
                 question.answers.forEach(answer => Object.setPrototypeOf(answer, Answer.prototype));
-            })
-            return quiz;
+            });
+            quiz = jsonQuiz;
         }
-
-        const quiz = this.buildQuizzFromSlides(deck);
-        quiz.role = role;
-        console.log(quiz);
-
+        else{
+            console.log('Loading the quiz from the slides markdown.');
+            quiz = this.buildQuizzFromSlides(deck);
+            quiz.role = role;
+        }
         // listen to events to save the quiz when needed
         deck.on("questionAnswered", () => {
+            console.log('Saving quiz data to sessionStorage');
             storage.setItem('quiz', JSON.stringify(quiz));
         });
-
         return quiz;
     }
 
