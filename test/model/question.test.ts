efed0fc374f21d7c7a1edbf685798a71c32e232b
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-02-03 15:03:33 +0100

    ✅ : add test for question parsing

---
+++ test/model/question.test.ts
@@ -0,0 +1,30 @@
+import { Question } from '../../src/model/question';
+import * as chai from 'chai'
+const expect = chai.expect;
+
+describe('model/question', () => {
+  it('should parse markdown questions', () => {
+    const markdown = `
+      - Who is Darth Sidious master ?
+      - [ ] Darth Bane
+      - [ ] Darth Tenebrous
+      - [x] Darth Plagueis
+    `;
+    const parsedQuestion = Question.fromMarkdown(markdown);
+
+    expect(parsedQuestion.text).to.equal('Who is Darth Sidious master ?');
+
+    expect(parsedQuestion.answers).to.have.lengthOf(3);
+
+    const [bane, tenebrous, plagueis] = parsedQuestion.answers;
+
+    expect(bane.text).to.equal('Darth Bane');
+    expect(bane.correct).to.be.false;
+
+    expect(tenebrous.text).to.equal('Darth Tenebrous');
+    expect(tenebrous.correct).to.be.false;
+
+    expect(plagueis.text).to.equal('Darth Plagueis');
+    expect(plagueis.correct).to.be.true;
+  });
+});
