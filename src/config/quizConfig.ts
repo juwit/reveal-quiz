d267b15772ce4149a44396d035f502bd046972a3
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-02-09 17:10:53 +0100

    ✨ : add randomizeAnswers configuration parameter

--- src/config/quizConfig.ts
+++ src/config/quizConfig.ts
@@ -1,11 +1,13 @@
 export interface QuizConfig {
   useTimer: boolean;
   timerDuration?: number;
+  randomizeAnswers: boolean;
 }
 
 export class DefaultQuizConfig implements QuizConfig {
   useTimer = false
   timerDuration = 60
+  randomizeAnswers = false
 
   merge(config: QuizConfig) {
     if (config.useTimer) {
@@ -13,6 +15,9 @@ export class DefaultQuizConfig implements QuizConfig {
     }
     if (config.timerDuration) {
       this.timerDuration = config.timerDuration
+    }
+    if(config.randomizeAnswers){
+      this.randomizeAnswers = config.randomizeAnswers
     }
   }
 }
